4c8d9538a640108b68dbdf0f3db67009bcedca5b
Spiffpitt <34319870+spiffpitt@users.noreply.github.com>
2019-03-26 11:26:14 -0500

    Added eslint

---
+++ .eslintrc.js
@@ -0,0 +1,41 @@
+module.exports = {
+  "env": {
+    "browser": true,
+    "es6": true,
+    "node": true
+  },
+  "extends": [
+    "eslint:recommended",
+    "plugin:vue/recommended"
+  ],
+  "parserOptions": {
+    "ecmaVersion": 2018,
+    "sourceType": "module"
+  },
+  "rules": {
+    "eol-last": ["error", "always"],
+    "indent": ["error", "tab", { "SwitchCase": 1 }],
+    "keyword-spacing": ["error", { "after": true, "before": true }],
+    "linebreak-style": ["error", "unix"],
+    "no-console": ["error", { "allow": ["log", "warn", "error"] }],
+    "no-trailing-spaces": "error",
+    "quotes": ["error", "double"],
+    "semi": ["error", "always"],
+    "space-before-blocks": ["error", "always"],
+    "vue/html-closing-bracket-newline": ["warn", {
+      "singleline": "never",
+      "multiline": "never"
+    }],
+    "vue/html-indent": ["error", "tab"],
+    "vue/max-attributes-per-line": ["warn", {
+      "singleline": 4,
+      "multiline": {
+        "max": 1,
+        "allowFirstLine": false
+      }
+    }],
+    "vue/no-use-v-if-with-v-for": ["warn", {
+      "allowUsingIterationVar": true
+    }]
+  }
+};
