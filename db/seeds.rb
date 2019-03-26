ce8f8e8f6c50eb7fd32d8f9ff042b1869982735e
Spiffpitt <34319870+spiffpitt@users.noreply.github.com>
2019-03-26 12:13:20 -0500

    updated seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,19 +1,12 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-AdminUser.destroy_all
+# AdminUser.destroy_all
+# AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
+
 Product.destroy_all
 ProductCategory.destroy_all
 
-AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
-
-ProductStatusCode.create(name: 'Normal', description: 'This marks a normal product.')
-ProductStatusCode.create(name: 'New', description: 'This marks a new product.')
-ProductStatusCode.create(name: 'Sale', description: 'This marks a product on sale.')
+normal = ProductStatusCode.create(name: 'Normal', description: 'This marks a normal product.')
+new = ProductStatusCode.create(name: 'New', description: 'This marks a new product.')
+sale = ProductStatusCode.create(name: 'Sale', description: 'This marks a product on sale.')
 
 ProductCategory.create(name: "Standard", description: "The standard mealdog category.")
-	.products.create(name: "The Standard", description: "The standard mealdog.", price: 9.99)
+	.products.create(name: "The Standard", description: "The standard mealdog.", price: 9.99, product_status_code: normal)
